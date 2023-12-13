380d564093692e31e190e66891071740f3f38054
minseung1226 <minseung1226@naver.com>
2023-12-13 21:18:05 +0900

    group 스키마 개발 완료

--- Models/group.js
+++ Models/group.js
@@ -2,6 +2,13 @@ const mongoose=require("mongoose");
 
 const groupSchema=new mongoose.Schema(
     {
-        name
-    }
+        name:String,
+        parentGroupId:{type:mongoose.Schema.ObjectId,ref:"Group"},
+        members:[{type:mongoose.Schema.ObjectId,ref:"User"}],
+        admin:{type:mongoose.Schema.ObjectId,ref:"User"},
+        
+    },
+    {timestamps:true}
 )
+
+model.export=mongoose.model("Group",groupSchema);
